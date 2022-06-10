5bce3f9a1b749c1413f36f61d781c095127b0fbc
PercyHoquee <percyhoquee@gmail.com>
2022-06-10 11:56:20 +0200

    input output function week 1 with initial classes

---
+++ code/input_output.py
@@ -0,0 +1,29 @@
+import csv
+from classes import Protein
+
+
+def create():
+    string = input() 
+    return Protein(string)
+
+
+def output(protein):
+    with open("output.csv", "w") as f:
+        writer = csv.writer(f)
+
+        writer.writerow(["amino", "fold"])
+
+        for amino in protein.aminos:
+            writer.writerow([amino.letter, amino.fold])
+
+        writer.writerow(["score", 0])
+    return
+
+
+def main():
+    protein = create()
+    output(protein)
+
+
+if __name__ == "__main__":
+    main()
