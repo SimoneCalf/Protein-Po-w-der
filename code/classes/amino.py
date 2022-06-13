53a9a7202b7188d0fc9a59aebc2d96f170a7ee11
PercyHoquee <percyhoquee@gmail.com>
2022-06-13 11:33:26 +0200

    create classes folder

---
+++ code/classes/amino.py
@@ -0,0 +1,6 @@
+class Amino:
+    def __init__(self, letter):
+        self.letter = letter
+        self.fold = 0
+
+    # set fold
---
+++ code/classes/protein.py
@@ -0,0 +1,6 @@
+class Protein:
+    def __init__(self, string):
+        def create_amino(letter):
+            return Amino(letter)
+
+        self.aminos = map(create_amino, string)
