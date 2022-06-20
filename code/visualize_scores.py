b3031ac82bf4eca4c4324df582d4bc5ea7f156a1
PercyHoquee <percyhoquee@gmail.com>
2022-06-20 12:12:32 +0200

    template for bar chart from dictionary

--- code/visualize_scores.py
+++ code/visualize_scores.py
@@ -1,4 +1,22 @@
+import matplotlib.pyplot as plt
+import random
 
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+
+score_freq = {}
+
+for i in range(5):
+    # score = random()
+    # if score in score_freq:
+        # score_freq[score] += 1
+    # else:
+        # score_freq[score] = 1
+    score_freq[random.randint(1,10)] = random.randint(1, 10)
+scores = score_freq.keys()
+frequencies = score_freq.values()
+ax.bar(scores, frequencies)
+plt.show()
 
 
 
