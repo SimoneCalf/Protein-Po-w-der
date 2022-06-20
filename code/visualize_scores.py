0bef203cec4d02bda254b72ea982e7a835ec13e8
PercyHoquee <percyhoquee@gmail.com>
2022-06-20 11:45:13 +0200

    empty visualize_scores.py

--- code/visualize_scores.py
+++ code/visualize_scores.py
@@ -1,24 +1,6 @@
-import matplotlib.pyplot as plt
-import random
-
-score_freq = {}
-
-for i in range(5):
-    score = random.randint(1, 1000)
-
-    if score in score_freq:
-        score_freq[score] += 1
-    else: 
-        score_freq[score] = 1
-
-scores = list(score_freq.keys())
-frequencies = list(score_freq.values())
-
-plt.bar(scores, frequencies)
-plt.show()
 
 
 
-    
+
 
 
